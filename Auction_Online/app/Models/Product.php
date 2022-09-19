64bf2eccdeaecdd8e9ead210e7c77a9b46f4cdfd
Chutiendat26092003 <chutiendat148@gmail.com>
2022-09-19 12:45:39 +0700

    delete brand

--- Auction_Online/app/Models/Product.php
+++ Auction_Online/app/Models/Product.php
@@ -13,11 +13,7 @@ class Product extends Model
     protected $table = 'products';
     protected $primaryKey = 'id';
     protected $guarded = [];
-
-    public function brand() {
-        return $this->belongsTo(Brand::class, 'brand_id', 'id');
-    }
-
+    
     public function productCategory() {
         return $this->belongsTo(ProductCategory::class, 'product_category_id', 'id');
     }
