07fab0b9242a347188ce5ad75d6770a0f317b536
Chutiendat26092003 <chutiendat148@gmail.com>
2022-09-20 21:47:43 +0700

    admin

--- Auction_Online/resources/views/admin/layout/master.blade.php
+++ Auction_Online/resources/views/admin/layout/master.blade.php
@@ -207,21 +207,21 @@
             <li class="nav-item dropdown pe-3">
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="./admin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-                    <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                    <img src="front/img/user/{{ \Illuminate\Support\Facades\Auth::user()->avatar ?? 'default-avatar.jpg'}}" alt="Profile" class="rounded-circle">
+                    <span class="d-none d-md-block dropdown-toggle ps-2">{{ \Illuminate\Support\Facades\Auth::user()->name }}</span>
                 </a><!-- End Profile Iamge Icon -->
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                     <li class="dropdown-header">
-                        <h6>Kevin Anderson</h6>
-                        <span>Web Designer</span>
+                        <h6>{{ \Illuminate\Support\Facades\Auth::user()->name }}</h6>
+                        <span>{{ \App\Utilities\Constant::$user_level[\Illuminate\Support\Facades\Auth::user()->level] }}</span>
                     </li>
                     <li>
                         <hr class="dropdown-divider">
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                        <a class="dropdown-item d-flex align-items-center" href="admin/user/{{ \Illuminate\Support\Facades\Auth::user()->id }}">
                             <i class="bi bi-person"></i>
                             <span>My Profile</span>
                         </a>
@@ -251,7 +251,7 @@
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
+                        <a class="dropdown-item d-flex align-items-center" href="admin/logout">
                             <i class="bi bi-box-arrow-right"></i>
                             <span>Sign Out</span>
                         </a>
@@ -318,7 +318,7 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; Copyright <strong><span>Auction Online</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Auction Online</span></strong>
     </div>
 </footer><!-- End Footer -->
 
--- Auction_Online/resources/views/admin/login.blade.php
+++ Auction_Online/resources/views/admin/login.blade.php
@@ -2,21 +2,23 @@
 <html lang="en">
 
 <head>
+    <base href="/">
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Login - Auction Online</title>
+    <title> Login - Auction Online</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
     <!-- Favicons -->
-    <link href="./admin/assets/img/favicon.png" rel="icon">
-    <link href="./admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="./front/img/Logo.png" rel="icon">
+    <link href="./front/img/Logo.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
     <link href="./admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -30,93 +32,85 @@
     <!-- Template Main CSS File -->
     <link href="./admin/assets/css/style.css" rel="stylesheet">
 
-    <!-- =======================================================
-    * Template Name: NiceAdmin - v2.3.1
-    * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-    * Author: BootstrapMade.com
-    * License: https://bootstrapmade.com/license/
-    ======================================================== -->
 </head>
 
 <body>
 
-<main>
-    <div class="container">
+    <main>
+        <div class="container">
 
-        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-                        <div class="d-flex justify-content-center py-4">
-                            <a href="admin/home" class="logo d-flex align-items-center w-auto">
-                                <img src="./admin/assets/img/logo.png" alt="">
-                                <span class="d-none d-lg-block">NiceAdmin</span>
-                            </a>
-                        </div><!-- End Logo -->
+                            <div class="d-flex justify-content-center py-4">
+                                <a href="admin/home" class="logo d-flex align-items-center w-auto">
+                                    <span class="d-none d-lg-block">Auction Online</span>
+                                </a>
+                            </div><!-- End Logo -->
 
-                        <div class="card mb-3">
+                            <div class="card mb-3">
 
-                            <div class="card-body">
+                                <div class="card-body">
 
-                                <div class="pt-4 pb-2">
-                                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                                    <p class="text-center small">Enter your username & password to login</p>
-                                </div>
+                                    <div class="pt-4 pb-2">
+                                        <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                        <p class="text-center small">Enter your email & password to login</p>
+                                    </div>
 
-                                <form class="row g-3 needs-validation" novalidate>
+                                    <form method="post" class="row g-3 needs-validation">
+                                        @csrf
+                                        @include('admin.components.notification')
 
-                                    <div class="col-12">
-                                        <label for="yourUsername" class="form-label">Username</label>
-                                        <div class="input-group has-validation">
-                                            <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                            <input type="text" name="username" class="form-control" id="yourUsername"
+                                        <div class="col-12">
+                                            <label for="email" class="form-label">Email</label>
+                                            <div class="input-group has-validation">
+                                                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                                <input type="email" name="email" class="form-control" id="email"
+                                                       required>
+                                                <div class="invalid-feedback">Please enter your username.</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <label for="yourPassword" class="form-label">Password</label>
+                                            <input type="password" name="password" class="form-control" id="yourPassword"
                                                    required>
-                                            <div class="invalid-feedback">Please enter your username.</div>
+                                            <div class="invalid-feedback">Please enter your password!</div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-12">
-                                        <label for="yourPassword" class="form-label">Password</label>
-                                        <input type="password" name="password" class="form-control" id="yourPassword"
-                                               required>
-                                        <div class="invalid-feedback">Please enter your password!</div>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="remember" value="true"
-                                                   id="rememberMe">
-                                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        <div class="col-12">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember" value="true"
+                                                       id="rememberMe">
+                                                <label class="form-check-label" for="rememberMe">Remember me</label>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <button class="btn btn-primary w-100" type="submit">Login</button>
-                                    </div>
-                                </form>
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                                        </div>
+                                    </form>
 
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="credits">
-                            <!-- All the links in the footer should remain intact. -->
-                            <!-- You can delete the links only if you purchased the pro version. -->
-                            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                        </div>
+                            <div class="credits">
+                                &copy; Copyright <strong><span>Auction Online</span></strong>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </section>
+            </section>
 
-    </div>
-</main><!-- End #main -->
+        </div>
+    </main><!-- End #main -->
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-        class="bi bi-arrow-up-short"></i></a>
+
+
+<script type="text/javascript" src="./admin/assets/js/jquery-3.2.1.min.js"></script>
 
 <!-- Vendor JS Files -->
 <script src="./admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
@@ -129,7 +123,9 @@
 <script src="./admin/assets/vendor/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
-<script src="./admin/assets/js/main.js"></script>
+<script type="text/javascript" src="./admin/assets/js/main.js"></script>
+<script type="text/javascript" src="./admin/assets/js/my_script.js"></script>
+
 
 </body>
 
